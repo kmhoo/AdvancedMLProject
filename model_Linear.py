a164dea7587036bb8ad8dea93ffa62e800c0c024
Griffin Okamoto <griffinokamoto@gmail.com>
2015-03-04 15:20:40 -0800

    added bagged regressions

--- model_Linear.py
+++ model_Linear.py
@@ -2,32 +2,46 @@ __author__ = 'griffin'
 
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 from data_processing import numpyArrays
 from sklearn.cross_validation import KFold
-from sklearn.preprocessing import scale
 from sklearn.metrics import mean_squared_error
-from sklearn.linear_model import SGDRegressor
 from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import BaggingRegressor
 
 
 # Import the data
 training = pd.read_csv("training_init.csv")
 X, y = numpyArrays(training)
-#X_scale = scale(X)
 
-# Correlation Matrix
-
-print np.corrcoef(X[:, :12], rowvar=0)
 
 # Set parameters
 model = LinearRegression()
 n = len(y)
-
-# Perform 5-fold cross validation
 scores = []
 kf = KFold(n, n_folds=5, shuffle=True)
 
+# Perform 5-fold cross validation
+# Calculate root mean squared error for train/test for each fold
+for train_idx, test_idx in kf:
+    X_train, X_test = X[train_idx], X[test_idx]
+    y_train, y_test = y[train_idx], y[test_idx]
+    model.fit(X_train, y_train)
+    prediction = model.predict(X_test)
+    rmse = np.sqrt(mean_squared_error(y_test, prediction))
+    scores.append(rmse)
+
+print "Linear Model 5-Fold CV"
+print scores
+print np.mean(scores)
+
+
+## Bagged OLS Regressions
+
+# Set parameters
+model = BaggingRegressor(LinearRegression())
+scores = []
+
+# Perform 5-fold cross validation
 # Calculate root mean squared error for train/test for each fold
 for train_idx, test_idx in kf:
     X_train, X_test = X[train_idx], X[test_idx]
@@ -39,7 +53,6 @@ for train_idx, test_idx in kf:
     rmse = np.sqrt(mean_squared_error(y_test, prediction))
     scores.append(rmse)
 
-print "Linear Model 5-Fold CV"
+print "Bagged Linear Model 5-Fold CV"
 print scores
 print np.mean(scores)
-
