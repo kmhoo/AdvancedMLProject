46bdc836e9caeb2a1b105fe2cbe634a8841937f6
Kailey Hoo <kmhoo027@gmail.com>
2015-03-05 08:47:08 -0800

    updates

--- FeatureEngineering/featureEngineering_userClustering.py
+++ FeatureEngineering/featureEngineering_userClustering.py
@@ -4,22 +4,22 @@ import pandas as pd
 from data_processing import updateColumns, roughImpute
 from sklearn.cluster import KMeans
 
-training = pd.read_csv("yelp_training.csv")
+training = pd.read_csv("../yelp_training.csv")
 training = updateColumns(training)
 training = roughImpute(training)
 
-print training
-columns = ['u_votes_cool', 'u_votes_funny', 'u_votes_useful', 'u_review_count', 'u_stars']
-X = training.loc[:, columns]
-print X
+X = training.loc[:, ['u_votes_cool', 'u_votes_funny', 'u_votes_useful', 'u_review_count']]
 
 clusters = [2, 3, 4, 5, 8, 10, 15, 20, 25, 50]
 init = [10, 30, 50, 100]
 
+cluster_list = []
 for cl in clusters:
-    print cl
+    iter = {}
+    iter['num_of_clusters'] = cl
     model = KMeans(n_clusters=cl)
     model.fit(X)
-    k_means_labels = model.labels_
-    print k_means_labels
-    print model.score(X)
+    iter['labels'] = model.labels_
+    iter['centroids'] = model.cluster_centers_
+    cluster_list.append(iter)
+print cluster_list
