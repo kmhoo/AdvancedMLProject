1baac1c7b366d33569c59527e946b173bc9727f2
Kailey Hoo <kmhoo027@gmail.com>
2015-03-09 20:11:19 -0700

    inputs and return pandas df

--- FeatureEngineering/applyNewFeatures.py
+++ FeatureEngineering/applyNewFeatures.py
@@ -7,29 +7,29 @@ from featureEngineering_userClustering import userCluster
 from featureEngineering_textFeatures import ngram_processing
 from featureEngineering_CollaborativeFiltering import filtering
 
-def applyFeatures(training_data, test_data, features_list, user_id_train, user_id_test):
+def applyFeatures(training_data, test_data, features_list):
 
     # Create numpy arrays
-    X_train, y_train = numpyArrays(training_data)
-    X_test, y_test = numpyArrays(test_data)
+    # X_train, y_train = numpyArrays(training_data)
+    # X_test, y_test = numpyArrays(test_data)
 
     if "Category Reduction" in features_list:
         # Reduce business categories using PCA
         category_col_indices = [idx for idx, col in enumerate(training_data.columns) if 'b_categories_' in col]
-        X_train, X_test = reduceCategories(X_train, X_test, category_col_indices)
+        training_data, X_test = reduceCategories(training_data, test_data, category_col_indices)
         print "Added Category Reduction"
 
     if "User Clustering" in features_list:
         # Add in column for user clusters
-        X_train, X_test = userCluster(X_train, X_test, user_id_train)
+        training_data, test_data = userCluster(training_data, test_data)
         print "Added User Clustering"
 
     if "Text Features" in features_list:
-        X_train, X_test = ngram_processing(X_train, X_test, user_id_train, user_id_test)
+        training_data, test_data = ngram_processing(training_data, test_data)
         print "Added Text Features"
 
     if "Collaborative Filtering" in features_list:
-        X_train, X_test = filtering(X_train, X_test, user_id_train, user_id_test)
+        training_data, test_data = filtering(training_data, test_data)
         print "Added Collaborative Filtering"
 
-    return X_train, X_test
+    return training_data, test_data
