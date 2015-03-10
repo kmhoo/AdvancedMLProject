238cab8a357b622b74f396c5c355725092fd34d1
Kailey Hoo <kmhoo027@gmail.com>
2015-03-09 17:35:37 -0700

    model updates

--- Models/model2_Boosting.py
+++ Models/model2_Boosting.py
@@ -6,8 +6,11 @@ from data_processing import numpyArrays
 from sklearn.ensemble import AdaBoostRegressor
 from sklearn.cross_validation import KFold
 from sklearn.metrics import mean_squared_error
+from itertools import combinations
+from FeatureEngineering.applyNewFeatures import applyFeatures
 
-def Round1(X, y):
+
+def round1(X, y):
     # Set parameters
     model = AdaBoostRegressor()
     n = len(y)
@@ -25,19 +28,49 @@ def Round1(X, y):
         rmse = np.sqrt(mean_squared_error(y_test, prediction))
         scores.append(rmse)
 
-    print scores
-    print np.mean(scores)
     return scores
 
 if __name__ == "__main__":
     # Import the data
     training = pd.read_csv("../training_2.csv")
+    test = pd.read_csv("../testing_2.csv")
 
+    # Strip out user_id with index
+    user_id = training['user_id']
+    user_id_test = test['user_id']
+
+    # Strip out business_id with index
+    bus_id = training['business_id']
+    bus_id_test = test['business_id']
+
+    # Drop user_id from dataframes
     training.drop('user_id', axis=1, inplace=True)
+    training.drop('business_id', axis=1, inplace=True)
+    test.drop('user_id', axis=1, inplace=True)
+    test.drop('business_id', axis=1, inplace=True)
 
-    X, y = numpyArrays(training)
+    # Create numpy arrays
+    X_train, y_train = numpyArrays(training)
+    X_test, y_test = numpyArrays(test)
 
     print "Boosting: AdaBoost Model 5-Fold CV"
 
-    r1_scores = Round1(X, y)
+    r1_scores = round1(X_train, y_train)
     print r1_scores
+    print np.mean(r1_scores)
+
+    ### TEST MODEL WITH DIFFERENT FEATURES
+
+    # Test on individual features
+    feature_eng = ['Category Reduction', 'User Clustering', 'Text Features', 'Collaborative Filtering']
+    for i in range(1, 5):
+        combo = combinations(feature_eng, i)
+        for com in combo:
+            feature_list = list(com)
+            print feature_list
+            X_train, X_test = applyFeatures(training, test, feature_list, user_id, user_id_test)
+            print "Added features listed above"
+
+            feature_scores = round1(X_train, y_train)
+            print "Scores:", feature_scores
+            print "Average Score:", np.mean(feature_scores)
--- Models/model2_RandomForest.py
+++ Models/model2_RandomForest.py
@@ -6,6 +6,8 @@ from data_processing import numpyArrays
 from sklearn.cross_validation import KFold
 from sklearn.metrics import mean_squared_error
 from sklearn.ensemble import RandomForestRegressor
+from itertools import combinations
+from FeatureEngineering.applyNewFeatures import applyFeatures
 
 def round1(X, y):
     # Set parameters
@@ -24,19 +26,48 @@ def round1(X, y):
         prediction = model.predict(X_test)
         rmse = np.sqrt(mean_squared_error(y_test, prediction))
         scores.append(rmse)
-    print scores
-    print np.mean(scores)
     return scores
 
 if __name__ == "__main__":
     # Import the data
     training = pd.read_csv("../training_2.csv")
+    test = pd.read_csv("../testing_2.csv")
 
+    # Strip out user_id with index
+    user_id = training['user_id']
+    user_id_test = test['user_id']
+
+    # Strip out business_id with index
+    bus_id = training['business_id']
+    bus_id_test = test['business_id']
+
+    # Drop user_id from dataframes
     training.drop('user_id', axis=1, inplace=True)
+    training.drop('business_id', axis=1, inplace=True)
+    test.drop('user_id', axis=1, inplace=True)
+    test.drop('business_id', axis=1, inplace=True)
 
-    X, y = numpyArrays(training)
+    # Create numpy arrays
+    X_train, y_train = numpyArrays(training)
+    X_test, y_test = numpyArrays(test)
 
     print "Random Forest Model 5-Fold CV"
 
-    r1_scores = round1(X, y)
+    r1_scores = round1(X_train, y_train)
     print r1_scores
+    print np.mean(r1_scores)
+
+    ### TEST MODEL WITH DIFFERENT FEATURES
+
+    # Test on individual features
+    feature_eng = ['Category Reduction', 'User Clustering', 'Text Features', 'Collaborative Filtering']
+    for i in range(1, 5):
+        combo = combinations(feature_eng, i)
+        for com in combo:
+            feature_list = list(com)
+            print feature_list
+            X_train, X_test = applyFeatures(training, test, feature_list, user_id, user_id_test)
+            print "Added features listed above"
+            feature_scores = round1(X_train, y_train)
+            print "Scores:", feature_scores
+            print "Average Score:", np.mean(feature_scores)
