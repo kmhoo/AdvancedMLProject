8b8905489a8244effc490c53d5340139b72d0062
Jeffrey Baker <jeff.baker.22@gmail.com>
2015-03-10 09:36:02 -0700

    more updates

--- FeatureEngineering/featureEngineering_textFeatures.py
+++ FeatureEngineering/featureEngineering_textFeatures.py
@@ -16,16 +16,19 @@ def get_reviews(fname):
     :param fname: file name of data set; expecting csv
     :return: pandas dataframe of text reviews (strings)
     """
+
     try:
         with open(fname, "rb") as infile:
             df = pd.DataFrame.from_csv(infile, header=0, index_col=False)
             print "# of user reviews: ", len(df)
+            # retrieve only the columns we care about
+            new_df = df[['user_id', 'r_text']]
             # drop any review entries that are blank
-            df = df.dropna()
+            new_df = new_df.dropna()
             # remove all newline characters from each entry
-            df['r_text'] = df['r_text'].str.replace('\n', ' ')
-            print "... after dropping NAs: ", len(df)
-        return df
+            new_df['r_text'] = new_df['r_text'].str.replace('\n', ' ')
+            print "... after dropping NAs: ", len(new_df)
+        return new_df
     except:
         raise IOError
 
@@ -83,69 +86,13 @@ def text_preprocessing(textString):
     return tokens
 
 
-def ngram_processing(trainText, testText):
-    """
-    Function to create array of data for processing in an algorithm.
-    :param userIDs: list of users, ordered alphabetically (per 'sorted' corpus df)
-    :param corpus: list of reviews by each user
-    :param userReviewCounts: dictionary of {user_id : inverse review frequency} to weigh frequent reviewers less
-    :return: pandas dataframe, where rows=users, columns=ngram features
-    """
-    dataSet = []
-
-    print "Starting text feature extraction "
-
-    # process review data from training data
-    reviews = get_reviews(trainText)
-    userIDlist, corpus, userReviewCounts = create_corpus(reviews)
-
-    testReviews = get_reviews(testText)
-    testUsrIDlist, testCorpus, testUsrRevCounts = create_corpus(testReviews)
-
-    # create tfidf transformer object
-    transformer = TfidfTransformer()
-
-    # calculate tf for bigram/trigram features; set minimum document frequency to 1000
-    ngram_vectorizer = CountVectorizer(ngram_range=(2,3), token_pattern=r'\b\w+\b', min_df=1000)
-
-    # get counts of the bigrams across documents; create an array for
-    print "fitting/vectorizing ngram features (this may take awhile)"
-    text_features = ngram_vectorizer.fit_transform(corpus).toarray()
-
-    # get feature names and their indices via 'vocabulary_'; result is a dictionary
-    feature_names = ngram_vectorizer.get_feature_names()
-    # print feature_names
-
-    # get vectorized features from the test data set
-    print "Vectorizing test reviews"
-    testNgram_vectors = CountVectorizer(token_pattern=r'\b\w+\b', vocabulary=ngram_vectorizer.vocabulary_)
-
-    testNgram_features = testNgram_vectors.fit_transform(testCorpus).toarray()
-
-    # calculate tfidf scores for the ngram features; results in sparse matrix
-    tfidf = transformer.fit_transform(text_features).toarray()
-
-    # apply review frequency normalization for each user's Ngram tfidfs
-    n = 0
-    for user in userIDlist:
-        # create new array of the tfidf normalized values for each row (user) in the tfidf array
-        dataSet.append([userReviewCounts[user]* elem for elem in tfidf[n]])
-        # reviewWeights.append(userReviewCounts[user])
-        n += 1
-
-    dataSet = np.array(dataSet)
-
-    # print dataSet[:5]
-
-
-    return dataSet
-
-
 def create_corpus(reviews):
     """
     Function that takes in a dataframe or array of data and collapses the reviews into single strings by user_id
-    :param df: data frame of user_ids and reviews
-    :return:
+    :param reviews: data frame of user_ids and reviews
+    :return: userIDlist - list of the user_ids, sorted alphabetically ascending
+             corpus - array of combined reviews by user
+             userReviewCounts - dictionary of {user_id : inverse review frequency} to weigh frequent reviewers less
     """
     corpus = []
     userIDlist = []
@@ -161,14 +108,14 @@ def create_corpus(reviews):
         userReviewCounts[k] = 1/float(v)
 
     # aggregate reviews by user ("document"); each user should now have a list of their reviews
-    userReviewLists = reviews.groupby('user_id')['r_text'].apply(list)       # output is a pd.Series sorted by user_id
+    # output is a pd.Series sorted by user_id
+    userReviewLists = reviews.groupby('user_id')['r_text'].apply(list)
 
     # recast user review lists as dictionary values
     for usr, rlist in userReviewLists.iteritems():
         userReviews[usr] = rlist
 
     # print userReviewLists[:5]
-    # create user_id to sparse-matrix-index hash table
 
     # create a list of "cleaned-up" strings for ngram vectorizer processing
     # join all the review strings together, using a ridiculous stop word (to exclude as a feature) to signify the
@@ -188,6 +135,86 @@ def create_corpus(reviews):
     return userIDlist, corpus, userReviewCounts
 
 
+def ngram_processing(trainText, testText):
+    """
+    Function to create array of data for processing in an algorithm.
+    :param trainText, testText: training and test data; must include fields 'user_id' and 'r_text'
+    :return: pandas dataframe, where rows=users, columns=ngram features
+    """
+    dataSet = []
+
+    print "Starting text feature extraction "
+
+    # process review data from training data
+    reviews = get_reviews(trainText)
+    userIDlist, corpus, userReviewCounts = create_corpus(reviews)
+
+    testReviews = get_reviews(testText)
+    testUserIDlist, testCorpus, testUserRevCounts = create_corpus(testReviews)
+
+    # create frequency count vectorizer; set minimum document frequency to 1000
+    ngramVectorizer = CountVectorizer(ngram_range=(2,3), token_pattern=r'\b\w+\b', min_df=1000)
+
+    # calculate frequencies for bigram/trigram features
+    print "Fitting/vectorizing ngram features (this may take awhile)"
+    textFeatures = ngramVectorizer.fit_transform(corpus).toarray()
+
+    # get feature names and their indices via 'vocabulary_'; result is a dictionary
+    feature_names = ngramVectorizer.get_feature_names()
+    print "# features trained:", len(feature_names)
+
+    # get vectorized features from the test data set, using only the features created from the training data set
+    print "Vectorizing test reviews"
+    testNgramVectorizer = CountVectorizer(token_pattern=r'\b\w+\b', vocabulary=ngramVectorizer.vocabulary_)
+
+    testNgramFeatures = testNgramVectorizer.fit_transform(testCorpus).toarray()
+
+    # print testNgramFeatures
+
+    # calculate tfidf scores for the ngram features; results in sparse matrix
+    transformer = TfidfTransformer()
+    tfidf = transformer.fit_transform(textFeatures).toarray()
+
+    # create tfidf transformer object, repeat for test data
+    transformer2 = TfidfTransformer()
+    tfidf_test = transformer2.fit_transform(testNgramFeatures).toarray()
+
+    print "# features in test", len(testNgramVectorizer.get_feature_names())
+
+
+    # apply review frequency normalization for each user's Ngram tfidfs
+    n = 0
+    for user in userIDlist:
+        # create new array of the tfidf normalized values for each row (user) in the tfidf array
+        dataSet.append([userReviewCounts[user] * elem for elem in tfidf[n]])
+        # reviewWeights.append(userReviewCounts[user])
+        n += 1
+
+    dataSet =  attach_text_features(np.array(dataSet))
+
+    print dataSet[:5]
+
+    # MERGE NEW FEATURES TO TRAINING DATA 2
+    train_x = attach_text_features(dataSet)
+
+    return dataSet
+
+
+def attach_text_features(tfidfArray):
+    """
+    Function to join new text features to the original training/test data files
+    :param tfidfArray: array of text features; rows are user_ids, columns are text features
+    :return: pandas dataframe of complete training data to this point
+    """
+
+    trainingData = "training_2.csv"
+    testData = "test_2.csv"
+
+    # train_x =
+
+    # return train_x
+    pass
+
 
 if __name__=="__main__":
     trainText = "yelp_review_text.csv"
@@ -196,13 +223,8 @@ if __name__=="__main__":
     trainingData = "training_2.csv"
     testData = "test_2.csv"
 
+    ngram_processing(trainText, testText)
 
-    trainTextFeatures = ngram_processing(trainText, testText)
-    # reviews = get_reviews(trainText)
-
-    # return corpus of data
-    # userIDs, corpus = create_corpus(reviews)
-    # ngram_processing(userIDs, corpus)
 
     ####################################
     # BUSINESS REVIEW COUNT SECTION
